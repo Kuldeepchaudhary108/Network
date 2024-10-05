614bcb2d3e81b82528e413713c040c5bdc83c841
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2024-10-06 00:55:28 +0530

    add Navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -8,9 +9,42 @@ export default function Header() {
       </div>
       <div className="flex space-x-20 font-semibold">
         {" "}
-        <p>About</p>
+        {/* <p>About</p>
         <p>Event</p>
-        <p>Blogs</p>
+        <p>Blogs</p> */}
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) =>
+            `block py-2 pr-4 pl-3 duration-200
+                   ${
+                     isActive ? "text-orange-700" : "text-gray-700"
+                   } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 `
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to={"/event"}
+          className={({ isActive }) =>
+            `block py-2 pr-4 pl-3 duration-200
+                   ${
+                     isActive ? "text-orange-700" : "text-gray-700"
+                   } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 `
+          }
+        >
+          Event
+        </NavLink>
+        <NavLink
+          to={"/blog"}
+          className={({ isActive }) =>
+            `block py-2 pr-4 pl-3 duration-200
+                   ${
+                     isActive ? "text-orange-700" : "text-gray-700"
+                   } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 `
+          }
+        >
+          Blog
+        </NavLink>
       </div>
       <div className="flex space-x-4 mr-5 font-semibold">
         <button className="hover:bg-gray-200 transition-all duration-200 rounded-xl min-w-20 h-10 ">
