046de960c940a3843dae21020ed49e97fa5dba41
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2024-10-05 23:02:50 +0530

    uploading home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,101 @@
 import React from "react";
-
+import Header from "./Header";
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="bg-gray-300 min-h-screen">
+        <div>
+          <Header />
+        </div>
+        <div className="flex mt-7">
+          <div className="ml-16">
+            {" "}
+            <img src="./public/elements/image 1.png" alt="" />
+          </div>
+          <div className="text-6xl w-2/3  ml-48 mt-16">
+            <h1 className="text-[#720026]">Revolutionize</h1>
+            <h1 className="text-[#F06449]">
+              hackathon <br />
+              collaboration with
+            </h1>
+            <h1 className="text-[#720026]">
+              seamless <br /> connections{" "}
+              <span className="text-[#F06449]">and</span>
+            </h1>
+            <h1 className="text-[#720026]">discovery</h1>
+          </div>
+        </div>
+        <div className="ml-10 mt-28">
+          <h1 className="text-[#36382E] text-xl font-semibold flex ">
+            EVENT{" "}
+            <img
+              className="w-8 ml-2"
+              src="./public/elements/right-long-solid.svg"
+              alt="right arrow"
+            />
+          </h1>
+        </div>
+
+        <div className="flex justify-between items-center px-4 mx-auto max-w-[1200px] gap-6">
+          <div className="h-[550px] w-1/3 bg-white text-center rounded-lg p-4">
+            <img
+              className="w-[392px] h-[392px] min-h-80 rounded-xl mx-auto"
+              src="./public/elements/Thumbnail 1.png"
+              alt="GDG Cloud Kochi"
+            />
+            <h1 className="mt-10 text-xl font-semibold">GDG Cloud Kochi</h1>
+            <div className="flex justify-center items-center space-x-2 mt-3">
+              <img
+                className="h-5"
+                src="./public/elements/profile.png"
+                alt="Profile"
+              />
+              <p>2290</p>
+            </div>
+            <button className="bg-[#F06449] w-16 rounded-md mt-5 text-white">
+              +Join
+            </button>
+          </div>
+          <div className="h-[550px] w-1/3 bg-white text-center rounded-lg p-4">
+            <img
+              className="w-[392px] h-[392px] min-h-80 rounded-xl mx-auto"
+              src="./public/elements/Thumbnail 1.png"
+              alt="GDG Cloud Kochi"
+            />
+            <h1 className="mt-10 text-xl font-semibold">GDG Cloud Kochi</h1>
+            <div className="flex justify-center items-center space-x-2 mt-3">
+              <img
+                className="h-5"
+                src="./public/elements/profile.png"
+                alt="Profile"
+              />
+              <p>2290</p>
+            </div>
+            <button className="bg-[#F06449] w-16 rounded-md mt-5 text-white">
+              +Join
+            </button>
+          </div>
+          <div className="h-[550px] w-1/3 bg-white text-center rounded-lg p-4">
+            <img
+              className="w-[392px] h-[392px] min-h-80 rounded-xl mx-auto"
+              src="./public/elements/Thumbnail 1.png"
+              alt="GDG Cloud Kochi"
+            />
+            <h1 className="mt-10 text-xl font-semibold">GDG Cloud Kochi</h1>
+            <div className="flex justify-center items-center space-x-2 mt-3">
+              <img
+                className="h-5"
+                src="./public/elements/profile.png"
+                alt="Profile"
+              />
+              <p>2290</p>
+            </div>
+            <button className="bg-[#F06449] w-16 rounded-md mt-5 text-white">
+              +Join
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,46 @@
-import { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Signup from './components/Signup.jsx'
-import Layout from './Layout.jsx'
-import Home from './components/pages/Home.jsx'
-import store from './App/store.js'
-import Login from './components/Login.jsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Signup from "./components/Signup.jsx";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home.jsx";
+import store from "./App/store.js";
+import Login from "./components/Login.jsx";
 
 function App() {
-
   const [isAuthenticated, isUserAuthenticated] = useState(false);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path='/signup' element={<Signup isUserAuthenticated={isUserAuthenticated} />} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/' element={<Layout isAuthenticated={isAuthenticated} />}>
+        <Route
+          path="/signup"
+          element={<Signup isUserAuthenticated={isUserAuthenticated} />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout isAuthenticated={isAuthenticated} />}>
           <Route path="" element={<Home />} />
         </Route>
       </>
     )
-  )
+  );
 
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  )
+  );
 }
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
