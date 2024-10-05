a1ef8c9a6c5ec41a6f89cff80c799e24ddf2455a
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2024-10-06 00:01:24 +0530

    update them button

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-
+import React, { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import ThemeBtn from "./components/ThemBtt";
+import { ThemeProvider } from "./components/Them";
 function Layout() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  // actual theme change
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
   return (
-    <>
-        <Outlet/>
-    </>
-  )
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="">{<Outlet />}</div>
+    </ThemeProvider>
+  );
 }
 
-export default Layout
+export default Layout;
