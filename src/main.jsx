12c46895f7603e7b088f8ebd9f009e14207c199d
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2024-10-06 08:25:37 +0530

    update Team profile

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,6 @@ function App() {
           <Route path="" element={<Home />} />
           <Route path="/event" element={<Event />} />
           <Route path="/room" element={<EventDetails />} />
-         
         </Route>
       </>
     )
